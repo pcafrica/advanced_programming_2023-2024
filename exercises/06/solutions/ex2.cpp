dbc51f74fdab2a672de7db2d152dc1e340ad40e4
Pasquale Africa <pafrica@sissa.it>
2023-11-08 14:31:10 +0100

    Add solutions

---
+++ exercises/06/solutions/ex2.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <unordered_set>
+#include <vector>
+
+template <typename Container>
+void print(const std::string &string, const Container &cont) {
+  std::cout << string << ":" << std::endl;
+  for (const auto &v : cont) {
+    std::cout << v << " ";
+  }
+  std::cout << std::endl << std::endl;
+}
+
+int main() {
+  // Initialize random number generator.
+  std::default_random_engine gen(42);
+  std::uniform_int_distribution<unsigned int> distribution(0, 9);
+
+  // Generate vector of 100 random integers between 0 and 9.
+  std::vector<unsigned int> random_numbers(100);
+  for (unsigned int &num : random_numbers) {
+    num = distribution(gen);
+  }
+  print("Original vector", random_numbers);
+
+  // Sort the vector.
+  std::vector<unsigned int> sorted_numbers(random_numbers);
+  std::sort(sorted_numbers.begin(), sorted_numbers.end());
+  print("Sorted numbers", sorted_numbers);
+
+  // Remove duplicates while sorting.
+  std::vector<unsigned int> sorted_unique_numbers(random_numbers);
+  std::sort(sorted_unique_numbers.begin(), sorted_unique_numbers.end());
+  auto unique_end =
+      std::unique(sorted_unique_numbers.begin(), sorted_unique_numbers.end());
+  sorted_unique_numbers.erase(unique_end, sorted_unique_numbers.end());
+  print("Sorted unique numbers", sorted_unique_numbers);
+
+  // Remove duplicates without sorting.
+  std::vector<unsigned int> unsorted_unique_numbers;
+  std::unordered_set<unsigned int> unique_numbers(random_numbers.begin(),
+                                                  random_numbers.end());
+
+  for (const unsigned int &element : random_numbers) {
+    if (unique_numbers.find(element) != unique_numbers.end()) {
+      unique_numbers.erase(element);
+      unsorted_unique_numbers.push_back(element);
+    }
+  }
+
+  print("Unsorted unique numbers", unsorted_unique_numbers);
+
+  return 0;
+}

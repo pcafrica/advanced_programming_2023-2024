654facf489da84d35ab882aa0773dece8e0263f2
Pasquale Africa <pafrica@sissa.it>
2023-11-15 16:52:16 +0100

    Upload solutions

---
+++ exercises/07/solutions/ex2/gradient_descent/gradient_descent.cpp
@@ -0,0 +1,33 @@
+#include "gradient_descent.hpp"
+#include <exception>
+#include <iostream>
+
+void GradientDescent::fit(const std::vector<double> &x,
+                          const std::vector<double> &y) {
+  unsigned int i = 0;
+
+  for (; i < max_iterations; ++i) {
+    double loss = 0;
+    for (size_t j = 0; j < x.size(); ++j) {
+      const double prediction = weight * x[j] + bias;
+
+      const double error = prediction - y[j];
+      loss += error * error;
+
+      weight -= learning_rate * error * x[j];
+      bias -= learning_rate * error;
+    }
+
+    loss /= x.size();
+
+    if (loss < tolerance) {
+      break;
+    }
+  }
+
+  if (i == max_iterations) {
+    throw std::runtime_error("Gradient descent dit not converge!");
+  }
+}
+
+double GradientDescent::predict(const double &x) { return weight * x + bias; }
